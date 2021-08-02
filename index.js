c277341c0e2ef6348c70b288356224a37275e204
Cory <corystephens012@gmail.com>
2021-08-02 16:40:16 -0400

    Updated the HTML and JS; HTML is for Rick and Morty website; JS shows 10 character cards and a comment form submission.

--- index.js
+++ index.js
@@ -1,32 +1,73 @@
-<script src="https://www.amcharts.com/lib/3/ammap.js" type="text/javascript"></script>
-<script src="https://www.amcharts.com/lib/3/maps/js/usaHigh.js" type="text/javascript"></script>
-<script src="https://www.amcharts.com/lib/3/themes/light.js" type="text/javascript"></script>
-<div id="mapdiv" style="width: 1000px; height: 450px;"></div>
-<div style="width: 1000px; font-size: 70%; padding: 5px 0; text-align: center; background-color: #535364; margin-top: 1px; color: #B4B4B7;"><a href="https://www.amcharts.com/visited_states/" style="color: #B4B4B7;">Create your own visited states map</a> or check out the <a href="https://www.amcharts.com/" style="color: #B4B4B7;">JavaScript Charts</a>.</div>
-<script type="text/javascript">
-var map = AmCharts.makeChart("mapdiv",{
-type: "map",
-theme: "light",
-panEventsEnabled : true,
-backgroundColor : "#535364",
-backgroundAlpha : 1,
-zoomControl: {
-zoomControlEnabled : true
-},
-dataProvider : {
-map : "usaHigh",
-getAreasFromMap : true,
-areas :
-[]
-},
-areasSettings : {
-autoZoom : true,
-color : "#B4B4B7",
-colorSolid : "#84ADE9",
-selectedColor : "#84ADE9",
-outlineColor : "#666666",
-rollOverColor : "#9EC2F7",
-rollOverOutlineColor : "#000000"
+fetch("https://rickandmortyapi.com/api/character")
+.then(resp=> resp.json())
+.then(data=> someFunction(data))
+    
+
+
+
+
+function someFunction(data){
+    const shortCharArray = data.results.splice(0, 9)
+    shortCharArray.forEach(element => {
+
+    //create an input field for a new comment with id pf comment-form
+        const newForm = document.createElement("form")
+        newForm.id = "comment-form"
+        
+        //create a new button for submitting a comment
+        const formBtn = document.createElement("button")
+        formBtn.innerText = "Submit"
+
+        //add event listener to the submit button that appends the users text to the character card.
+        newForm.addEventListener("submit", (event) =>{
+            
+            event.preventDefault();
+            //append the users text to a new paragraph element to the card.
+            cardAppender(event)
+            // card.append(newP)
+            console.log(event.target)
+        })
+        
+        
+
+        const comment = document.createElement("input");
+        comment.className = "commentForm"
+        comment.placeholder = "Enter comment"
+
+        
+
+        newForm.append(comment,formBtn)
+
+
+        const charContainer = document.querySelector("#char-container")
+        const card = document.createElement("div")
+        card.className="char-card";
+
+        const name = document.createElement("h2")
+        name.textContent= element.name
+
+        const species = document.createElement("p")
+        species.textContent = `Species: ${element.species}`
+
+        const gender = document.createElement("p")
+        gender.textContent = `Gender: ${element.gender}`
+
+        const status = document.createElement("p")
+        status.textContent = `Status: ${element.status}`
+
+        card.append(name, species, gender, status, newForm)
+        charContainer.appendChild(card)
+
+
+    //function to append a new comment to the card.
+        function cardAppender(){
+            const newP = document.createElement("p")
+            newP.textContent = comment.value
+            card.append(newP)
+        }
+    });
+    // forEach item in shortCharArray = card render name, status, gender, comments, create image with char's id
+
+    
 }
-});
-</script>
+
